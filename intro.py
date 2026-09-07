4215c8a9f72c92e8b7325ceb61241e09ef612914
Krzysztof Pyrzak <krzpyr@kvn.no>
2026-09-07 15:09:55 +0200

    while loops

--- intro.py
+++ intro.py
@@ -1,15 +1,8 @@
-import random
-#gjette spill
-rettTall = random.randint(0, 1000)
+run = True
+count = 0
+while run:
+    print("hello")
+    if count > 10:
+        run = False
 
-#10forsøk 
-for x in range (10):
-    gjett = int(input("gjett et tall mellom 0 og 1000: "))
-    if gjett == rettTall:
-        print("yay")
-        break
-    elif gjett < rettTall:
-        print("høyere!")
-    else:
-        print("lavere!")
-    print("du har", 9 - x, "forsøk igjenn")
+    count =+ 5
